c0afa3d69db6fb7921ee402ff0c334df3ac433a9
Dasha <darya.gapeeva.01@gmail.com>
2024-02-19 20:48:36 +0300

    feat: added button component

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,14 +1,34 @@
 import React, { FC, ReactNode } from 'react';
+import buttonStyles from './buttonStyles.module.css';
+import container from './container.module.css';
+
 
 export interface ButtonProps {
-    children:ReactNode;
-    color:string;
+    children?:ReactNode | string;
+    className?:string;
+    variant?: 'text' | 'contained' | 'outlined';
+    isDisabled?:boolean;
+    size?:'small' | 'medium' | 'large';
+    onClick?:()=>void;
 }
 
-const Button:FC<ButtonProps> = ({children, color, ...props}) => {
+const Button:FC<ButtonProps> = ({children, onClick, className, variant='contained', isDisabled, size='medium', ...props}) => {
+    const checkingTypes = (value:string) => {
+        return buttonStyles[value]
+    };
+    
+    const typeVariant = checkingTypes(variant);
+    const typeSize =checkingTypes(size);
+   
+    const stringCheck = typeof children == 'string';
+    
     return (
-        <button {...props} style={{color}}>
-            {children}
+        <button  
+        className = {`${container.button} ${typeVariant} ${typeSize}  ${className ?? ''}`} 
+        disabled={isDisabled}
+        onClick={onClick} 
+        {...props}> 
+            {stringCheck ? children.toUpperCase() : children}
         </button>
     )
 }

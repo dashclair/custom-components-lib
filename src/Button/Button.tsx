36f4a17e0c4e6a280f0d1fabcd7cdd18dc9553d1
Dasha <darya.gapeeva.01@gmail.com>
2024-02-21 12:15:44 +0300

    refactor: edited button structure

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,36 +1,37 @@
-import React, { FC, ReactNode } from 'react';
-import buttonStyles from './buttonStyles.module.css';
-import container from './container.module.css';
+import React from "react";
+import buttonStyles from "./Button.module.css";
+import { ButtonProps } from "./Button.types";
 
-
-export interface ButtonProps {
-    children?:ReactNode | string;
-    className?:string;
-    variant?: 'text' | 'contained' | 'outlined';
-    isDisabled?:boolean;
-    size?:'small' | 'medium' | 'large';
-    onClick?:()=>void;
-}
-
-const Button:FC<ButtonProps> = ({children, onClick, className, variant='contained', isDisabled, size='medium', ...props}) => {
-    const checkingTypes = (value:string) => {
-        return buttonStyles[value]
+const Button = ({
+    children,
+    onClick,
+    className,
+    variant = "contained",
+    isDisabled,
+    size = "medium",
+    ...props
+}: ButtonProps) => {
+    const checkingTypes = (value: string) => {
+        return buttonStyles[value];
     };
-    
+
     const typeVariant = checkingTypes(variant);
-    const typeSize =checkingTypes(size);
-   
-    const stringCheck = typeof children == 'string';
-    
+    const typeSize = checkingTypes(size);
+
+    const stringCheck = typeof children == "string";
+
     return (
-        <button  
-        className = {`${container.button} ${typeVariant} ${typeSize}  ${className ?? ''}`} 
-        disabled={isDisabled}
-        onClick={onClick} 
-        {...props}> 
+        <button
+            className={`${buttonStyles.button} ${typeVariant} ${typeSize}  ${
+                className ?? ""
+            }`}
+            disabled={isDisabled}
+            onClick={onClick}
+            {...props}
+        >
             {stringCheck ? children.toUpperCase() : children}
         </button>
-    )
-}
+    );
+};
 
 export default Button;

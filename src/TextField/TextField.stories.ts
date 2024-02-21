5d4fd4d53d2dad5a7818ddf83580d297b086b3f9
Dasha <darya.gapeeva.01@gmail.com>
2024-02-21 12:06:42 +0300

    refactor: replaced stories files

--- src/TextField/TextField.stories.ts
+++ src/TextField/TextField.stories.ts
@@ -1,20 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import TextField from '../TextField/TextField';
+import TextField from './TextField';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Example/TextField',
   component: TextField,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  parameters: {layout:'centered'},
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  // argTypes: {
-  //   // color: { control: 'color' },
-  // },
 } satisfies Meta<typeof TextField>;
 
 export default meta;

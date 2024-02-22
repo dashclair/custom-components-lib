83b0feb22dee3f87e7ebd1c3538a51fbe9d7d39f
Dasha <darya.gapeeva.01@gmail.com>
2024-02-22 12:45:04 +0300

    feat: created select component

---
+++ src/Select/Select.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import TextField from "../TextField/TextField";
+import selectStyles from "./Select.module.scss";
+import { SelectProps } from "./Select.types";
+import Button from "../Button/Button";
+import arrow from './Vector.svg';
+
+const Select = ({
+    label,
+    options,
+    defaultValue = "",
+    selectError,
+}: SelectProps) => {
+    
+    const [open, setOpen] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(defaultValue);
+
+    const handleOpen = () => {
+        if (!selectedItem) {
+            setSelectedItem(" ");
+        }
+        setOpen(!open);
+    };
+
+    return (
+        <div className={selectStyles.select}>
+            <div className={selectStyles.selectContainer}>
+                <TextField
+                    value={selectedItem}
+                    variant="outlined"
+                    select={open}
+                    label={label}
+                    error={selectError}
+                    className={selectStyles.input}
+                />
+                <button
+                    onClick={handleOpen}
+                    className={selectStyles.selectButton}
+                >
+                    <img src={arrow} className={open ? selectStyles.arrowRotate : ''}/>
+                </button>
+            </div>
+            <div
+                className={
+                    open
+                        ? selectStyles.optionsContainer
+                        : selectStyles.optionsContainer_none
+                }
+            >
+                {open &&
+                    Array.isArray(options) &&
+                    options.map((option) => {
+                        return (
+                            <Button
+                                key={option}
+                                className={selectStyles.optionButton}
+                                variant="text"
+                                onClick={() => {
+                                    setSelectedItem(option);
+                                    setOpen(!open);
+                                }}
+                                select
+                            >
+                                {option}
+                            </Button>
+                        );
+                    })}
+            </div>
+        </div>
+    );
+};
+
+export default Select;

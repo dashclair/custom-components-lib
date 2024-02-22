a479d145ca9d831fae8fe6bad6fa3ad4b0662975
Dasha <darya.gapeeva.01@gmail.com>
2024-02-22 12:46:38 +0300

    feat: created select stories

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Button from './Button/Button';
 import TextField from './TextField/TextField';
+import Select from './Select/Select';
 
 export {
     Button,
-    TextField
+    TextField,
+    Select
 }

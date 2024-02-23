b25a221422dc27e794d0eff67aca04f9cc7c5b68
Dasha <darya.gapeeva.01@gmail.com>
2024-02-23 16:30:59 +0300

    feat: updated version and exported component

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import Button from './Button/Button';
 import TextField from './TextField/TextField';
 import Select from './Select/Select';
 import Checkbox from './Checkbox/Checkbox';
+import Switch from './Switch/Switch';
 
 export {
     Button,
     TextField,
     Select,
     Checkbox,
+    Switch
 }

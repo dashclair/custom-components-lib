a4241afe0160ddab577ecf7d3d34b72318f2948c
Dasha <darya.gapeeva.01@gmail.com>
2024-03-01 12:38:49 +0300

    fix: edit jest config for tests with svg imports

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -56,6 +56,7 @@ const Modal = ({
                 >
                     {hasCloseBtn && (
                         <button
+                            data-testid="modal-button"
                             className={styles.closeButton}
                             onClick={handleCloseModal}
                         >
